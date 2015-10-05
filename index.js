b93eca8fda8df2711bd674646728b2f29958c0c3
Tomasz Rakowski <tomasz@akayami.com>
2015-10-05 14:22:57 -0400

    Changed the way config is invoked to allow for path overwrite

--- index.js
+++ index.js
@@ -4,38 +4,45 @@ var appPath = process.env.PWD +path.sep;
 
 var config = {};
 
-if (argv.base) {
-	try {
-		config = require(argv.base);
-	} catch (e) {
-		console.error('Failed to load specified config: ' + argv.base);
-		console.error(e.message);
+module.exports  = function(appRoot) {
+
+	if(appRoot) {
+		console.info('Overwriting default application lookup with:' + appRoot);
+		appPath = appRoot;
 	}
-} else {
-	try {
-		var config = require(appPath + './conf/config');
-	} catch(e) {
-		console.error('Failed to find default config: ' + appPath + './conf/config');
-		console.error(e.message);
+
+	if (argv.base) {
+		try {
+			config = require(argv.base);
+		} catch (e) {
+			console.error('Failed to load specified config: ' + argv.base);
+			console.error(e.message);
+		}
+	} else {
+		try {
+			var config = require(appPath + './conf/config');
+		} catch(e) {
+			console.error('Failed to find default config: ' + appPath + './conf/config');
+			console.error(e.message);
+		}
 	}
+
+
+	if (argv.conf) {
+		try {
+			config = require(argv.conf)(config);
+		} catch(e) {
+			console.error('Failed to load specified extending config config: ' + argv.conf);
+			console.error(e.message);
+		}
+	} else {
+		try {
+			config = require(appPath + './conf/config-local.js')(config);
+		} catch(e) {
+			console.warn('Failed to load default extending config config: ' + appPath + './conf/config-local.js');
+			console.warn(e.message);
+			console.info('You can use --conf to extend base config and/or --base to pass the base config.');
+		}
+	}
+	return config;
 }
-
-
-if (argv.conf) {
-	try {
-		config = require(argv.conf)(config);
-	} catch(e) {
-		console.error('Failed to load specified extending config config: ' + argv.conf);
-		console.error(e.message);
-	}
-} else {
-	try {
-		config = require(appPath + './conf/config-local.js')(config);
-	} catch(e) {
-		console.warn('Failed to load default extending config config: ' + appPath + './conf/config-local.js');
-		console.warn(e.message);
-		console.info('You can use --conf to extend base config and/or --base to pass the base config.');
-	}
-}
-
-module.exports = config;
